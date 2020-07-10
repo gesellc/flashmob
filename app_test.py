cf24c324047c2ef990a1bf7e117f75008a9b4ac4
IceBreaker80 <33756179+IceBreaker80@users.noreply.github.com>
2020-07-10 21:03:04 +0200

    "mob next [ci-skip]"

--- app_test.py
+++ app_test.py
@@ -5,8 +5,10 @@ def test_example():
     assert "This" == "This"
 
 import requests
+import pyyaml
 
 def test_request():
+
     api_key = '<replace-with-your-api-key>'
     api_secret = '<replace-with-your-api-secret>'
     image_url = 'https://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg'
