63d4631778217001b7609b2a29f51e6ca13eb2f3
IceBreaker80 <33756179+IceBreaker80@users.noreply.github.com>
2020-07-23 20:44:30 +0200

    "mob next [ci-skip]"

---
+++ detect_fire.py
@@ -0,0 +1,36 @@
+import requests
+import yaml
+
+
+def detect_fire_on_image(picture_url):
+    api_response = call_api(picture_url)
+    confidence_of_tag_detection = get_tag_confidence(api_response, fire_tag)
+    return confidence_of_tag_detection
+
+
+def call_api(image_url):
+    with open(r'config.yml') as file:
+        config = yaml.load(file)
+    print(config)
+
+    api_key = config['k']
+    api_secret = config['s']
+
+    response = requests.get(
+        'https://api.imagga.com/v2/tags?image_url=%s' % image_url,
+        auth=(api_key, api_secret))
+
+    return response.json()
+
+
+def get_tag_confidence(api_response, tag):
+    #iterate over all available tags in api_response
+    #hold current confidence and tag
+    #return confidence for specified tag
+    print (api_response['result']['tags'][0])
+    for tag_entry in api_response['result']['tags']:
+        confidence = tag_entry['confidence']
+        confidence_tag = tag_entry['tag']['en']
+        if confidence_tag == tag:
+            return confidence
+    return 0
