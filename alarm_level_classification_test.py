cedb8e0832ff3e77b4ffa3e7f594aa554cf9098b
IceBreaker80 <33756179+IceBreaker80@users.noreply.github.com>
2020-07-30 20:39:21 +0200

    "mob next [ci-skip]"

--- alarm_level_classification_test.py
+++ alarm_level_classification_test.py
@@ -1,7 +1,10 @@
 def get_alarm_level(fire_confidence):
     # Todo: Triangulate sequential steps
     # ...
-    return 'green'
+    if fire_confidence == 0:
+        return 'green'
+    else:
+        return 'red'
 
 
 def test_level_green():
@@ -9,3 +12,7 @@ def test_level_green():
 
 def test_level_red():
     assert 'red' == get_alarm_level(100)
+    assert 'red' == get_alarm_level( 69)
+
+def test_level_yellow():
+    assert '' == get_alarm_level(68)
