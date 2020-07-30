5d92ed4c90d47df16cfe2e7889096e9061a487eb
lisihocke <lisihocke@users.noreply.github.com>
2020-07-30 20:51:59 +0200

    "mob next [ci-skip]"

---
+++ fire_person.py
@@ -0,0 +1,9 @@
+from alarm_level_classification_test import get_alarm_level
+from detect_fire import detect_fire
+
+image_url = 'https://images.unsplash.com/photo-1507019658682-2924f9dbd499?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+
+confidence = detect_fire(image_url)
+alarm_level = get_alarm_level(confidence)
+
+print(alarm_level)
