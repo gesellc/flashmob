fc8f2084f5bc0dff35c537277a31b895d1f41a08
IceBreaker80 <33756179+IceBreaker80@users.noreply.github.com>
2020-08-06 20:45:13 +0200

    "mob next [ci-skip]"

--- detect_fire_use_case_test.py
+++ detect_fire_use_case_test.py
@@ -1,5 +1,10 @@
 import pytest
 
 @pytest.mark.xfail
-def test_interface_call():
-    detect_fire_use_case(image_url)
+# fire  = 'https://images.unsplash.com/photo-1507019658682-2924f9dbd499?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+
+
+# maybe_fire = 'https://images.unsplash.com/photo-1528227436006-bf774de37e4d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80'
+def test_no_fire_detected():
+    no_fire_image_url = 'https://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg'
+    detect_fire_use_case(no_fire_image_url)
