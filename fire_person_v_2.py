8ada17240699c3c7af4f03a5d8e4c3c7417d0271
lisihocke <lisihocke@users.noreply.github.com>
2020-08-06 21:37:00 +0200

    "mob next [ci-skip]"

--- fire_person_v_2.py
+++ fire_person_v_2.py
@@ -2,18 +2,27 @@ from alarm_level_classification_test import get_alarm_level
 from detect_fire import detect_fire
 from detect_fire_use_case_test import detect_fire_use_case
 
-print('Please enter image url:')
-image_url = input()
+
+def get_image_url_use_case():
+    print('Please enter image url:')
+    return input()
+
+
+image_url = get_image_url_use_case()
 
 alarm_level = detect_fire_use_case(image_url)
 
-if alarm_level == 'red':
-    response = 'Your dedicated fire person says LOUDLY: "FIRE!"'
-elif alarm_level == 'green':
-    response = 'Your dedicated fire person says: "All good, nothing to worry about. :)"'
-else:
-    response = 'Your dedicated fire person says: "Hmm... do you smell anything?"'
 
-print(response)
+def print_alarm_level(level):
+    if level == 'red':
+        response = 'Your dedicated fire person says LOUDLY: "FIRE!"'
+    elif level == 'green':
+        response = 'Your dedicated fire person says: "All good, nothing to worry about. :)"'
+    else:
+        response = 'Your dedicated fire person says: "Hmm... do you smell anything?"'
+    print(response)
+
+
+print_alarm_level(alarm_level)
 
 
